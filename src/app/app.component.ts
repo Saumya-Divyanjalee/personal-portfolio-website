cfb2f66082f7f15a6bcdcece79ccc9210e5fe67b
saumy <saumyadivyanjalee529@gmail.com>
2026-09-14 20:26:49 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss ']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
