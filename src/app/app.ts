a44da57adc9dea952fc493fea93d26bd6ca630b9
saumy <saumyadivyanjalee529@gmail.com>
2026-09-15 12:08:40 +0530

    added footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, HostListener, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
-import { ThemeService } from './services/theme.service';
+import { ThemeService } from './services/theme';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   themeService = inject(ThemeService);
+  scrolled = signal(false);
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.scrolled.set(window.scrollY > 20);
+  }
 }
